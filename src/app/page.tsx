7c13362ab5f29deb26879ef9144502431a1b661f
jeanmax1me <jeanmaximegilbert@gmail.com>
2024-04-01 19:00:03 +0200

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/app/pages/Hero";
+import About from "./pages/About";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Hero />
+      <About />
+    </div>
+  );
+}
---
+++ src/app/pages/Hero.tsx
@@ -0,0 +1,11 @@
+import { LampDemo } from "@/app/components/ui/Lamp";
+import HeaderSection from "@/app/components/ui/Header"
+
+export default function Header() {
+  return (
+  <div>
+    <HeaderSection />
+    <LampDemo />
+  </div>
+  )
+}
